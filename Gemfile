fead1b45462b11fe9211389ed35d1c8bfcb78e0a
David McClure <davidwilliammcclure@gmail.com>
2015-08-17 13:51:58 -0700

    Stubbing in an Import::Step class.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'activerecord-postgis-adapter'
+gem 'progress_bar'
 
 group :development, :test do
 
---
+++ lib/import/step.rb
@@ -0,0 +1,51 @@
+
+class Import::Step
+
+  class << self
+    attr_accessor :depends
+  end
+
+  @depends = []
+
+  #
+  # Initialize the progress bar.
+  #
+  def initialize
+
+    if count
+      @bar = ProgressBar.new(count)
+    end
+
+  end
+
+  #
+  # How many items will the step process?
+  #
+  # @return [Integer]
+  #
+  def count
+    nil
+  end
+
+  #
+  # Increment the progress bar.
+  #
+  def increment
+    @bar.increment
+  end
+
+  #
+  # Run the step.
+  #
+  def up
+    raise NotImplementedError
+  end
+
+  #
+  # Reverse the step.
+  #
+  def down
+    raise NotImplementedError
+  end
+
+end
