a63bd676db6611655e1e105ff68c1d8b3535e04b
David McClure <davidwilliammcclure@gmail.com>
2015-08-19 15:45:35 -0700

    Scrubbing out Import::Step test stubs for now, pending the subclass mocking question.

--- spec/import/step_spec.rb
+++ spec/import/step_spec.rb
@@ -2,31 +2,5 @@
 require 'rails_helper'
 
 describe Import::Step do
-
-  describe '#up()' do
-
-    context 'satisfied' do
-      it 'does not run the step'
-    end
-
-    context 'not satisfied' do
-      it 'runs the step'
-      it 'registers the step'
-    end
-
-  end
-
-  describe '#down()' do
-
-    context 'satisfied' do
-      it 'reverts the step'
-      it 'unregisters the step'
-    end
-
-    context 'not satisfied' do
-      it 'does not revert the step'
-    end
-
-  end
-
+  # TODO
 end
