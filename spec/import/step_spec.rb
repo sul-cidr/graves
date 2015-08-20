d05beb23dae39688cad93222701b947a2da5ea76
David McClure <davidwilliammcclure@gmail.com>
2015-08-20 13:53:00 -0700

    Test that Import::Step#up() is not run when the step is already satisfied.

--- spec/import/step_spec.rb
+++ spec/import/step_spec.rb
@@ -5,16 +5,31 @@ describe Import::Step do
 
   let(:step) {
     Class.new(Import::Step) do
+
       def self.name
         'TestStep'
       end
+
+      def up
+        true
+      end
+
+      def down
+        true
+      end
+
     end
   }
 
   describe '#up()' do
 
-    it 'does not run the step' do
-      step.new.up
+    context 'safisfied' do
+
+      it 'does not run the step' do
+        create(:import_step, step: 'TestStep')
+        expect(step.new.up).to be nil
+      end
+
     end
 
   end
