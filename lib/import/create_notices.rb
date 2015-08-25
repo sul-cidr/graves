fcfe3a46f1c9b5dc40ff362214996a4c68044de1
David McClure <davidwilliammcclure@gmail.com>
2015-08-24 17:32:16 -0700

    Import all remaining Notice fields.

--- lib/import/create_notices.rb
+++ lib/import/create_notices.rb
@@ -6,10 +6,19 @@ module Import
       @DB[:master_20150601].distinct(:nid).each do |n|
 
         Notice.create(
-          pub_date: parse_date(n[:pub_date]),
-          notice_date: parse_date(n[:notification_date]),
-          deadline: parse_date(n[:deadline]),
-          url: n[:url],
+          pub_date:       parse_date(n[:pub_date]),
+          notice_date:    parse_date(n[:notification_date]),
+          deadline:       parse_date(n[:deadline]),
+          url:            clean(n[:url]),
+          site_p:         clean(n[:site_p]),
+          site_c:         clean(n[:site_c]),
+          title_p:        clean(n[:title_p]),
+          title_c:        clean(n[:title_c]),
+          org_p:          clean(n[:org_p]),
+          org_c:          clean(n[:org_c]),
+          contact_phone:  clean(n[:contact_phone]),
+          contact_p:      clean(n[:contact_p]),
+          contact_c:      clean(n[:contact_c]),
         )
 
       end
@@ -19,8 +28,24 @@ module Import
       Notice.delete_all
     end
 
+    #
+    # Parse a mm/dd/yyy date.
+    #
+    # @param string [String]
+    # @return [Date]
+    #
     def parse_date(string)
       Date.strptime(string.to_s, '%m/%d/%Y') rescue nil
+    end
+
+    #
+    # Convert empty strings and "N/A" to nil.
+    #
+    # @param string [String]
+    # @return [String|nil]
+    #
+    def clean(value)
+      ['', 'N/A'].include?(value) ? nil : value;
     end
 
   end
--- notes/import.md
+++ notes/import.md
@@ -20,18 +20,18 @@
 
 	- `pub_date` - Notice / pub_date
 	- `deadline` - Notice / deadline
-	- `site_c`
-	- `site_p`
+	- `site_c` - Notice / site_p
+	- `site_p` - Notice / site_c
 	- `url` - Notice / url
 	- `local`
-	- `title_c`
-	- `title_p`
-	- `org_c`
-	- `org_p`
+	- `title_c` - Notice / title_c
+	- `title_p` - Notice / title_p
+	- `org_c` - Notice / org_c
+	- `org_p` - Notice / org_p
 	- `notification_date` - Notice / notice_date
-	- `contact_c`
-	- `contact_p`
-	- `contact_phone`
+	- `contact_c` - Notice / contact_c
+	- `contact_p` - Notice / contact_p
+	- `contact_phone` - Notice / contact_phone
 	- `keyword`
 	- `database`
 	- `notes`
