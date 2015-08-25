cd3d039426b331abd45297beebe9a8687a6458cd
David McClure <davidwilliammcclure@gmail.com>
2015-08-25 10:09:15 -0700

    Stub in the beginning of the Collection import - link the new rows with notices.

--- lib/import/create_collections.rb
+++ lib/import/create_collections.rb
@@ -4,7 +4,13 @@ module Import
 
     def up
       @DB[:master_20150601].distinct(:rid).each do |r|
-        # TODO
+
+        notice = Notice.find_by(legacy_id: r[:nid])
+
+        Collection.create(
+          notice: notice
+        )
+
       end
     end
 
