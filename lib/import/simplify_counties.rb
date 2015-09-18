d5f2a851360fae9cb78d7d643fbfe7db98d8630c
David McClure <davidwilliammcclure@gmail.com>
2015-09-18 10:59:03 -0700

    Break long lines in shell commands in SimplifyCounties.

--- lib/import/simplify_counties.rb
+++ lib/import/simplify_counties.rb
@@ -27,28 +27,34 @@ module Import
     # Merge into a single shapefile.
     #
     def merge
-      `mapshaper */export.shp encoding=gb18030 combine-files -merge-layers -o counties-merged.shp`
+      `mapshaper */export.shp \
+      encoding=gb18030 combine-files -merge-layers \
+      -o counties-merged.shp`
     end
 
     #
     # Unproject the combined shapefile.
     #
     def unproject
-      `ogr2ogr --config SHAPE_ENCODING UTF-8 counties-unprojected.shp counties-merged.shp -t_srs EPSG:4326`
+      `ogr2ogr --config SHAPE_ENCODING UTF-8 \
+      counties-unprojected.shp counties-merged.shp \
+      -t_srs EPSG:4326`
     end
 
     #
     # Convert to simplified TopoJSON.
     #
     def simplify
-      `topojson counties-unprojected.shp -p -s 0.000001 -o counties-simplified.topojson`
+      `topojson counties-unprojected.shp -p -s 0.000001 \
+      -o counties-simplified.topojson`
     end
 
     #
     # Convert back into a shapefile.
     #
     def finalize
-      `mapshaper counties-simplified.topojson -o counties.shp`
+      `mapshaper counties-simplified.topojson \
+      -o counties.shp`
     end
 
   end
