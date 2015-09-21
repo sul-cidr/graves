ce10d016206e7a3a846e48d8e98c17e80c8ae1b5
David McClure <davidwilliammcclure@gmail.com>
2015-09-21 16:08:16 -0700

    Stubbing out tests for the Geometry concern, using with_model.

--- spec/models/concerns/geometry_spec.rb
+++ spec/models/concerns/geometry_spec.rb
@@ -3,8 +3,35 @@ require 'rails_helper'
 
 describe Geometry do
 
-  it 'test' do
-    expect(true).to be true
+  with_model :GeoModel do
+
+    table do |t|
+      t.string :field1
+      t.string :field2
+      t.string :field3
+      t.geometry :geometry, srid: 4326
+    end
+
+    model do
+      include Geometry
+    end
+
+  end
+
+  describe ':with_geojson' do
+
+    it 'selects `geometry` as GeoJSON' do
+
+      GeoModel.create!(geometry: Helpers::Geo.point(1, 2))
+
+      # Get first record, parse JSON.
+      geojson = MultiJson.load(GeoModel.with_geojson.first.geojson)
+      pp geojson
+
+    end
+
+    it 'limits coordinate digits'
+
   end
 
 end
