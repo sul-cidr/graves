48f5a2d476c11be7685786f9b6200ac9494d032a
David McClure <davidwilliammcclure@gmail.com>
2015-09-23 10:46:54 -0700

    Breaks out a separate <GeometricGroup /> component.

--- app/assets/javascripts/src/graves/components/county-group.js
+++ app/assets/javascripts/src/graves/components/county-group.js
@@ -4,13 +4,15 @@ import L from 'leaflet';
 import { connect } from 'react-redux';
 import React, { Component, PropTypes, findDOMNode } from 'react';
 import d3 from 'd3-browserify';
+
+import GeometricGroup from './geometric-group';
 import * as actions from '../actions/counties';
 
 
 @connect(state => ({
   geojson: state.counties.geojson
 }))
-export default class extends Component {
+export default class extends GeometricGroup {
 
 
   static contextTypes = {
@@ -27,34 +29,6 @@ export default class extends Component {
   /**
    * Inject the d3 rig.
    */
-  componentWillMount() {
-
-    let map = this.context.map;
-
-    // Inject the SVG containers.
-    let pane = this.context.map.getPanes().overlayPane;
-    this.svg = d3.select(pane).append('svg');
-    this.g = this.svg.append('g').classed('leaflet-zoom-hide', true);
-
-    let origin = map.getPixelOrigin();
-
-    // Map lon/lat -> layer pixels.
-    let transform = d3.geo.transform({
-      point: function(x, y) {
-        let point = map.project(new L.LatLng(x, y))._subtract(origin);
-        this.stream.point(point.x, point.y);
-      }
-    });
-
-    // Path generator.
-    this.path = d3.geo.path().projection(transform);
-
-  }
-
-
-  /**
-   * Inject the d3 rig.
-   */
   componentDidUpdate() {
 
     // Render the counties.
@@ -64,15 +38,6 @@ export default class extends Component {
       .append('path')
       .classed({ county: true })
       .attr('d', this.path)
-
-    // Cache the data extent.
-    this.bounds = d3.geo.path()
-      .projection(null)
-      .bounds(this.props.geojson);
-
-    // Sync <paths> with map.
-    this.context.map.on('viewreset', this.sync.bind(this, false));
-    this.sync(true);
 
     let idMap = {};
     this.counties.each(function(f) {
@@ -92,67 +57,7 @@ export default class extends Component {
       this.props.dispatch(actions.unhighlightCounty(c.id));
     });
 
-  }
-
-
-  /**
-   * Sync the counties with the map.
-   *
-   * @param {Boolean} first
-   */
-  sync(first) {
-
-    // Top right.
-    let tl = this.context.map.latLngToLayerPoint([
-      this.bounds[0][0],
-      this.bounds[0][1],
-    ]);
-
-    // Bottom left.
-    let br = this.context.map.latLngToLayerPoint([
-      this.bounds[1][0],
-      this.bounds[1][1],
-    ]);
-
-    // Position the container.
-    this.svg
-      .attr('width', br.x-tl.x)
-      .attr('height', tl.y-br.y)
-      .style('top', `${br.y}px`)
-      .style('left', `${tl.x}px`);
-
-    if (first) {
-
-      // Set the initial <g> offset.
-      this.g.attr('transform', `translate(${-tl.x},${-br.y})`);
-
-      // Cache the starting corners.
-      this.tl0 = tl;
-      this.br0 = br;
-
-    }
-
-    else {
-
-      let t = d3.transform(this.g.attr('transform'));
-
-      // Scale to match zoom level.
-      t.scale = [
-        t.scale[0] * ((br.x - tl.x) / (this.br.x - this.tl.x)),
-        t.scale[1] * ((tl.y - br.y) / (this.tl.y - this.br.y)),
-      ];
-
-      t.translate = [-this.tl0.x, -this.br0.y];
-
-      let scale = `scale(${t.scale.join()})`;
-      let translate = `translate(${t.translate.join()})`;
-      this.g.attr('transform', scale+translate);
-
-    }
-
-    // Cache the current corners.
-    this.tl = tl;
-    this.br = br;
+    super.componentDidUpdate();
 
   }
 
