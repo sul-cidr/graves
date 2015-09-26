104a6cebead8ab98c94e8258ac58b41d457487d4
David McClure <davidwilliammcclure@gmail.com>
2015-09-25 17:40:29 -0700

    Experimenting with the home screen styling.

--- app/assets/javascripts/src/graves/components/home.js
+++ app/assets/javascripts/src/graves/components/home.js
@@ -12,8 +12,12 @@ export default class extends Component {
   render() {
     return (
       <div className="home">
+
         <h1 className="logo">Chinese Graves</h1>
-        <h6>By Tom Mullaney and CIDR</h6>
+        <p className="byline">By Tom Mullaney and CIDR</p>
+
+        <p className="blurb">The Chinese Graves Project is a digital humanities initiative based at Stanford that examines the phenomenon of grave relocation in modern China, a campaign that has led to the exhumation and reburial of 10 million corpses in the past decade alone, and has transformed China’s graveyards into sites of acute personal, social, political, and economic contestation.</p>
+
       </div>
     );
   }
