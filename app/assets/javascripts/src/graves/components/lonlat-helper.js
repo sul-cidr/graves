cb7c37720917631e4ca9c6f07dd77bd04a36574d
David McClure <davidwilliammcclure@gmail.com>
2015-09-28 09:46:10 -0700

    When the map is clicked in edit mode, 'bounce' the lon/lat listing.

--- app/assets/javascripts/src/graves/components/lonlat-helper.js
+++ app/assets/javascripts/src/graves/components/lonlat-helper.js
@@ -1,6 +1,7 @@
 
 
 import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
 
 
 export default class extends Component {
@@ -21,6 +22,7 @@ export default class extends Component {
     super(props);
 
     this.state = {
+      flash: false,
       lon: 0,
       lat: 0,
     };
@@ -32,12 +34,32 @@ export default class extends Component {
    * Listen for cursor events.
    */
   componentDidMount() {
+
     this.context.map.on('mousemove', e => {
       this.setState({
         lon: e.latlng.lng.toFixed(3),
         lat: e.latlng.lat.toFixed(3),
       });
     });
+
+    this.context.map.on('click', () => {
+      this.bounce();
+    });
+
+  }
+
+
+  /**
+   * "Bounce" the lon/lat display.
+   */
+  bounce() {
+
+    this.setState({ flash: true });
+
+    setTimeout(() => {
+      this.setState({ flash: false });
+    }, 1000);
+
   }
 
 
@@ -45,11 +67,17 @@ export default class extends Component {
    * Render the cursor position.
    */
   render() {
+
+    let cx = classNames('lonlats', 'animated', {
+      bounceIn: this.state.flash
+    });
+
     return (
-      <div className="lonlats">
+      <div className={cx}>
         {this.state.lon}, {this.state.lat}
       </div>
     );
+
   }
 
 
