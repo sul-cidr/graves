3aab58aaad002064159be40c1e7abbcd96f1432a
David McClure <davidwilliammcclure@gmail.com>
2015-09-28 21:42:57 -0700

    Dispatch showChoropleth() actions from the dropdown.

--- app/assets/javascripts/src/graves/components/choropleth-control.js
+++ app/assets/javascripts/src/graves/components/choropleth-control.js
@@ -2,9 +2,12 @@
 
 import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/counties';
 import codes from '../data/cdc-codes.yml';
 
 
+@connect(null, actions)
 export default class extends Component {
 
 
@@ -14,15 +17,27 @@ export default class extends Component {
   render() {
 
     let options = _.map(codes.counties, (label, code) => {
-      return <option value={code}>{label}</option>
+      return <option key={code} value={code}>{label}</option>
     });
 
+    let onChange = this.onChange.bind(this);
+
     return (
-      <select className="choropleth">
+      <select onChange={onChange} className="choropleth">
         {options}
       </select>
     );
 
+  }
+
+
+  /**
+   * When the code is changed.
+   *
+   * @param {Object} e
+   */
+  onChange(e) {
+    this.props.showChoropleth(e.target.value);
   }
 
 
--- app/assets/javascripts/src/graves/components/county-choropleth.js
+++ app/assets/javascripts/src/graves/components/county-choropleth.js
@@ -2,27 +2,17 @@
 
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import * as actions from '../actions/counties';
 
 
-@connect(
-  state => ({
-    code: state.counties.choropleth
-  }),
-  actions
-)
+@connect(state => ({
+  code: state.counties.choropleth
+}))
 export default class extends Component {
 
 
   static propTypes = {
     g: PropTypes.array.isRequired,
     code: PropTypes.any,
-  }
-
-
-  // TODO|dev
-  componentDidMount() {
-    this.props.showChoropleth('a100001_10');
   }
 
 
