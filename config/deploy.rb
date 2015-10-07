26410acc735b5eb99b93a852d6489e3d8621cf45
David McClure <davidwilliammcclure@gmail.com>
2015-10-07 11:49:10 -0700

    Add 'data' to the list of linked files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,11 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push(
+  'config/secrets.yml',
+  'config/database.yml',
+  'data',
+)
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
