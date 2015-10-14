9955ba264fa61cb5b639075e6667d3f5c9e195e3
David McClure <davidwilliammcclure@gmail.com>
2015-10-14 10:28:02 -0700

    Stub in an <AnchorMarkup /> component.

--- app/assets/javascripts/src/components/narrative.js
+++ app/assets/javascripts/src/components/narrative.js
@@ -5,8 +5,9 @@ import React, { Component, PropTypes, findDOMNode } from 'react';
 import * as actions from '../actions/narrative';
 
 import Spinner from './spinner';
-import CollectionMarkup from './collection-markup';
 import SectionMarkup from './section-markup';
+import CollectionMarkup from './collection-markup';
+import AnchorMarkup from './anchor-markup';
 import HighlightLine from './highlight-line';
 
 
@@ -98,6 +99,7 @@ export default class extends Component {
 
           <SectionMarkup markup={this.state.markup} />
           <CollectionMarkup markup={this.state.markup} />
+          <AnchorMarkup markup={this.state.markup} />
 
           <HighlightLine />
 
