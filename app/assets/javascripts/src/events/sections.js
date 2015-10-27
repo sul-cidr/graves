004e1c58a0b01eed9ad6d0a56b33ed402f628ef7
David McClure <davidwilliammcclure@gmail.com>
2015-10-27 13:39:58 -0700

    Add a selectSection() event.

--- app/assets/javascripts/src/events/sections.js
+++ app/assets/javascripts/src/events/sections.js
@@ -5,6 +5,7 @@ import Radio from 'backbone.radio';
 import {
   SECTIONS,
   SCROLL_SECTION,
+  SELECT_SECTION,
 } from '../constants';
 
 
@@ -18,4 +19,15 @@ const channel = Radio.channel(SECTIONS);
  */
 export function scrollSection(id) {
   channel.trigger(SCROLL_SECTION, id);
+}
+
+
+/**
+ * Select a collection.
+ *
+ * @param {Number} id
+ * @param {String} id
+ */
+export function selectSection(id, origin) {
+  channel.trigger(SELECT_SECTION, id);
 }
