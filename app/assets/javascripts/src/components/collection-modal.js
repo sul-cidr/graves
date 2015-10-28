b9e0a2f4a15250226f01e5fa048515c1a6d1353b
David McClure <davidwilliammcclure@gmail.com>
2015-10-28 16:13:30 -0700

    Connect <CollectionModal /> with the store.

--- app/assets/javascripts/src/components/collection-modal.js
+++ app/assets/javascripts/src/components/collection-modal.js
@@ -1,12 +1,16 @@
 
 
 import _ from 'lodash';
+import { connect } from 'react-redux';
 import React from 'react';
 import { Modal } from 'react-bootstrap'
 
 import Component from './component';
 
 
+@connect(state => ({
+  feature: state.collections.selected
+}))
 export default class extends Component {
 
 
@@ -14,11 +18,23 @@ export default class extends Component {
    * Render the collection metadata modal.
    */
   render() {
+
+    let show = Boolean(this.props.feature);
+
     return (
-      <Modal show={true} onHide={this.onHide}>
-        <h1>Collection</h1>
+      <Modal show={show} onHide={this.onHide}>
+
+        <Modal.Header closeButton>
+          <Modal.Title>Collection</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Metadata.</p>
+        </Modal.Body>
+
       </Modal>
     );
+
   }
 
 
