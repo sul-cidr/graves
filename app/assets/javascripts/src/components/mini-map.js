2a67ffa1e46940aa2f972c5fb73a3c124e2ee79e
David McClure <davidwilliammcclure@gmail.com>
2015-10-28 14:24:33 -0700

    Initial pass on rendering the extent rectangle.

--- app/assets/javascripts/src/components/mini-map.js
+++ app/assets/javascripts/src/components/mini-map.js
@@ -2,7 +2,7 @@
 
 import $ from 'jquery';
 import d3 from 'd3-browserify';
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { findDOMNode } from 'react-dom';
 import Component from './component';
 
@@ -10,6 +10,11 @@ import chinaJSON from '../data/CHN.geo.json';
 
 
 export default class extends Component {
+
+
+  static contextTypes = {
+    map: PropTypes.object.isRequired
+  }
 
 
   /**
@@ -21,6 +26,7 @@ export default class extends Component {
 
     this._plotChina();
     this._addMarkup();
+    this._listenForMove();
 
   }
 
@@ -30,7 +36,7 @@ export default class extends Component {
    */
   _plotChina() {
 
-    let bbox = this.svg.node().getBBox();
+    let bbox = this.svg.node().getBoundingClientRect();
 
     let offset = [
       bbox.width  / 2,
@@ -61,6 +67,43 @@ export default class extends Component {
     // Extent <rect>.
     this.extent = this.svg.append('rect')
       .classed({ extent: true });
+
+  }
+
+
+  /**
+   * Mirror the map pan state.
+   */
+  _listenForMove() {
+    this.context.map.on('move', this.setExtent.bind(this));
+    this.setExtent();
+  }
+
+
+  /**
+   * Mirror the map pan state.
+   */
+  setExtent() {
+
+    let tl = this.context.map.containerPointToLatLng([
+      0, 0
+    ]);
+
+    let br = this.context.map.containerPointToLatLng([
+      $(window).width(),
+      $(window).height()
+    ]);
+
+    let c1 = this.projection([tl.lng, tl.lat]);
+
+    let c2 = this.projection([br.lng, br.lat]);
+
+    this.extent.attr({
+      x:      c1[0],
+      y:      c1[1],
+      height: c2[1]-c1[1],
+      width:  c2[0]-c1[0],
+    });
 
   }
 
