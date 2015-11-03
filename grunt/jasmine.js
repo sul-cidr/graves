6c159faee4b3f5f1e85d625e791c0e6e0fd5b4ea
David McClure <davidwilliammcclure@gmail.com>
2015-11-02 16:50:43 -0800

    Load the CSS payload into the Jasmine tests.

--- grunt/jasmine.js
+++ grunt/jasmine.js
@@ -5,7 +5,8 @@ export default {
   graves: {
     src: '<%= dist.js %>/graves.js',
     options: {
-      specs: '<%= dist.js %>/graves-test.js'
+      specs: '<%= dist.js %>/graves-test.js',
+      styles: '<%= dist.css %>/graves.css',
     }
   }
 
