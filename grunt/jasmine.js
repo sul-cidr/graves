06ce44e014e4b3ec2cce67f24124f7c3d6a5c7c3
David McClure <davidwilliammcclure@gmail.com>
2015-11-02 16:57:57 -0800

    Use a custom runner template that sets a window.JASMINE=true variable.

--- grunt/jasmine.js
+++ grunt/jasmine.js
@@ -3,11 +3,15 @@
 export default {
 
   graves: {
+
     src: '<%= dist.js %>/graves.js',
+
     options: {
       specs: '<%= dist.js %>/graves-test.js',
       styles: '<%= dist.css %>/graves.css',
+      template: '<%= src.js %>/graves/test/graves.tmpl',
     }
+
   }
 
 };
