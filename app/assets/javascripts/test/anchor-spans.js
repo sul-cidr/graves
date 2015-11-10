a16e27c5d3e4b2c64bd2947a1efad9c06dec48ac
David McClure <davidwilliammcclure@gmail.com>
2015-11-10 12:48:43 -0800

    Test highlight line on anchor spans.

--- app/assets/javascripts/test/anchor-spans.js
+++ app/assets/javascripts/test/anchor-spans.js
@@ -1,6 +1,10 @@
 
 
+import $ from 'jquery';
 import * as utils from './utils';
+
+import dataFocusNarrativeJSON from
+'./fixtures/anchor-spans/data-focus.narrative.json';
 
 
 describe('Anchor Spans', function() {
@@ -9,9 +13,26 @@ describe('Anchor Spans', function() {
     utils.navigate('/read/narrative');
   });
 
-  describe('data-id', function() {
-    it('shows a highlight line on hover');
+  describe('data-focus', function() {
+
+    beforeEach(function() {
+      utils.respondNarrative(dataFocusNarrativeJSON);
+    });
+
+    it('shows a highlight line on hover', function() {
+
+      // Hover on the span.
+      $('.anchor:first-child').trigger('mouseenter');
+      let line = $('#map-line line');
+
+      expect(line).toBeInDOM();
+
+      // TODO: Test position?
+
+    });
+
     it('focuses the map on click');
+
   });
 
 });
