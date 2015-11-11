6fc623b7b464f904be814346108e492d99944d2b
David McClure <davidwilliammcclure@gmail.com>
2015-11-10 16:52:32 -0800

    Test section mounting layer counts.

--- app/assets/javascripts/test/sections.js
+++ app/assets/javascripts/test/sections.js
@@ -1,11 +1,35 @@
 
 
 import * as utils from './utils';
+import SectionGroup from '../src/components/section-group';
+
+import validSectionsJSON from
+'./fixtures/sections/valid.narrative.json';
 
 
 describe('Sections', function() {
 
-  it('adds map layers when a narrative loads');
+  let group;
+
+  beforeEach(function() {
+    group = utils.unwrap(GRAVES, SectionGroup);
+  });
+
+  it('adds map layers when a narrative loads', function() {
+
+    expect(group.labels.getLayers().length).toEqual(0);
+    expect(group.boxes.getLayers().length).toEqual(0);
+
+    utils.navigate('/read/narrative');
+    utils.respondNarrative(validSectionsJSON);
+
+    expect(group.labels.getLayers().length).toEqual(10);
+    expect(group.boxes.getLayers().length).toEqual(10);
+
+    // test labels, coords
+
+  });
+
   it('removes map layers when a narrative closes');
   it('highlights the label of the visible section');
   it('shows a zoom tip for unfocused section');
