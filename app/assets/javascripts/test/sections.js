9a51014afabe3dcc0689934ac082ebf4f60b9a22
David McClure <davidwilliammcclure@gmail.com>
2015-11-11 10:36:15 -0800

    Closing out section tests, for now.

--- app/assets/javascripts/test/sections.js
+++ app/assets/javascripts/test/sections.js
@@ -77,9 +77,4 @@ describe('Sections', function() {
 
   });
 
-  it('highlights the label of the visible section');
-  it('shows a zoom tip for unfocused section');
-  it('does not show a zoom tip for a focused section');
-  it('zooms to the section on click');
-
 });
