645da1f35144b632b0b95dde133b1978fc9cd3ee
David McClure <davidwilliammcclure@gmail.com>
2015-11-11 19:43:05 -0800

    Bind <Content /> to the route reducer.

--- app/assets/javascripts/src/components/content.js
+++ app/assets/javascripts/src/components/content.js
@@ -4,12 +4,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
+import * as actions from '../actions/route';
 import Component from './component';
+import Narrative from './narrative';
 
 
-@connect(state => ({
-  spatial: state.narrative.spatial
-}))
+@connect(state => state.route)
 export default class extends Component {
 
 
