7265151b44940bc3317cba22d7daeb1f596ed112
David McClure <davidwilliammcclure@gmail.com>
2016-02-07 11:08:35 -0800

    Pull in the home.css payload in index.html.slim.

--- grunt/less.js
+++ grunt/less.js
@@ -8,7 +8,12 @@ export default {
 
   read: {
     src: '<%= src.css %>/read/index.less',
-    dest: '<%= dist.css %>/read.css'
-  }
+    dest: '<%= dist.css %>/read.css',
+  },
+
+  home: {
+    src: '<%= src.css %>/home/index.less',
+    dest: '<%= dist.css %>/home.css',
+  },
 
 };
