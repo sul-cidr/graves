67914a315adf0bc3965afac3d33e551b7057a238
David McClure <davidwilliammcclure@gmail.com>
2016-02-09 11:18:45 -0800

    Stub in <App />.

---
+++ app/assets/javascripts/read/components/app.js
@@ -0,0 +1,17 @@
+
+
+import React, { Component } from 'react';
+
+
+export default class extends Component {
+
+
+  /**
+   * Render the top-level application structure.
+   */
+  render() {
+    return <h1>graves</h1>;
+  }
+
+
+}
--- app/assets/javascripts/read/index.js
+++ app/assets/javascripts/read/index.js
@@ -1,3 +1,14 @@
 
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './typography';
+
+import App from './components/app';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('read'),
+);
