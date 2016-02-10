ba0f31ce2578d57028ae76526621b4b10873e70d
David McClure <davidwilliammcclure@gmail.com>
2016-02-10 10:59:56 -0800

    Stub in a <CollectionMarkers /> component.

---
+++ app/assets/javascripts/read/components/collection-markers.js
@@ -0,0 +1,30 @@
+
+
+import React, { Component } from 'react';
+
+
+export default class extends Component {
+
+
+  /**
+   * Initialize the feature group and ID map.
+   */
+  componentWillMount() {
+    // TODO
+  }
+
+
+  /**
+   * Request collections.
+   */
+  componentDidMount() {
+    // TODO
+  }
+
+
+  render() {
+    return null;
+  }
+
+
+}
