56d67c9df99c4245f7fb1a4c3b983c70667eb5e3
David McClure <davidwilliammcclure@gmail.com>
2016-02-15 11:34:06 -0800

    Affix the map when the scroll top >= height.

--- app/assets/javascripts/read/page/map-position.js
+++ app/assets/javascripts/read/page/map-position.js
@@ -19,7 +19,7 @@ function cacheHeight() {
  */
 function positionMap() {
   let top = $(window).scrollTop();
-  $('body').toggleClass('fixed', top > height);
+  $('body').toggleClass('fixed', top >= height);
 }
 
 
