5bb1b16f5476c46c2288419e048fdfc7bc565900
David McClure <davidwilliammcclure@gmail.com>
2016-02-15 09:53:18 -0800

    Stub in a <MapMenu /> container.

---
+++ app/assets/javascripts/read/components/map-menu.js
@@ -0,0 +1,21 @@
+
+
+import React, { Component } from 'react';
+
+
+export default class extends Component {
+
+
+  /**
+   * Render the map burger menu.
+   */
+  render() {
+    return (
+      <div id="map-menu">
+        <h3>Map Menu</h3>
+      </div>
+    );
+  }
+
+
+}
