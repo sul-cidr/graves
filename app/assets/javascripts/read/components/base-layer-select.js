0e533fda4eff8524601a6756f5c25cc6ed352418
David McClure <davidwilliammcclure@gmail.com>
2016-02-16 13:26:20 -0800

    Connect the base layer select with the store.

--- app/assets/javascripts/read/components/base-layer-select.js
+++ app/assets/javascripts/read/components/base-layer-select.js
@@ -5,15 +5,19 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Select from 'react-select';
 
+import * as actions from '../actions/base-layer';
+
 
 @connect(
-  state => state.baseLayer
+  state => state.baseLayer,
+  actions,
 )
 export default class extends Component {
 
 
   static propTypes = {
     layerId: PropTypes.number.isRequired,
+    changeBaseLayer: PropTypes.func.isRequired,
   };
 
 
@@ -31,11 +35,26 @@ export default class extends Component {
 
     return (
       <Select
+
+        clearable={false}
         options={options}
+
+        onChange={this.onChange.bind(this)}
         value={this.props.layerId}
+
       />
     );
 
+  }
+
+
+  /**
+   * When the base layer is changed.
+   *
+   * @param {Object} option
+   */
+  onChange(option) {
+    this.props.changeBaseLayer(option.value);
   }
 
 
