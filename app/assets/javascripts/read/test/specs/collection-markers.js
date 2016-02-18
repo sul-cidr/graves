c6ca37f8548fc121c06df0cc8cc77c8f7c143b56
David McClure <davidwilliammcclure@gmail.com>
2016-02-18 14:34:53 -0800

    Test click on collection, show modal.

--- app/assets/javascripts/read/test/specs/collection-markers.js
+++ app/assets/javascripts/read/test/specs/collection-markers.js
@@ -1,5 +1,7 @@
 
 
+import $ from 'jquery';
+
 import CollectionMarkers from '../../src/components/collection-markers';
 
 import * as utils from '../utils';
@@ -7,6 +9,9 @@ import * as utils from '../utils';
 
 import addMarkersJSON from
 '../fixtures/collection-markers/collections/add-markers.json';
+
+import showModalJSON from
+'../fixtures/collection-markers/collections/show-modal.json';
 
 
 describe('Collection Markers', function() {
@@ -41,6 +46,19 @@ describe('Collection Markers', function() {
       lng: 5,
       lat: 6,
     });
+
+  });
+
+
+  it('shows the metadata modal on click', function() {
+
+    utils.respondCollections(showModalJSON);
+
+    markers.group.fire('click', {
+      layer: markers.idToMarker[1]
+    });
+
+    expect($('.modal.collection-1')).toBeVisible();
 
   });
 
--- spec/fixtures/collection_markers_spec.rb
+++ spec/fixtures/collection_markers_spec.rb
@@ -15,4 +15,14 @@ describe 'Collection Markers', type: :feature do
 
   end
 
+  it 'show-modal' do
+
+    create(:collection, id: 1, geometry: Helpers::Geo.point(0, 0))
+
+    visit('api/collections.json')
+
+    write_collection_fixture('collection-markers', 'show-modal', page)
+
+  end
+
 end
