6bcf316ca5f8e5e42f866f9ba0d334d9b63a9c23
David McClure <davidwilliammcclure@gmail.com>
2016-02-21 13:11:24 -0800

    Generate fixtures for the collection HTML data-id tests.

---
+++ spec/fixtures/collection_html_spec.rb
@@ -0,0 +1,42 @@
+
+require 'rails_helper'
+
+describe 'Collection HTML', type: :feature do
+
+  describe 'data-id' do
+
+    describe 'hover' do
+
+      it 'collection' do
+
+        create(
+          :collection,
+          id: 1,
+          geometry: Helpers::Geo.point(1, 1),
+        )
+
+        visit('api/collections.json')
+
+        write_collection_fixture('collection-html', 'data-id', page)
+
+      end
+
+      it 'narrative' do
+
+        markup = <<-HTML
+          <span class="collection" data-id="1">collection</span>
+        HTML
+
+        n = create(:narrative, markup: markup)
+
+        visit("read/#{n.slug}")
+
+        write_page_fixture('collection-html', 'data-id', page)
+
+      end
+
+    end
+
+  end
+
+end
