a931aef2685343c4b19f722b8cce9dae2330bb95
David McClure <davidwilliammcclure@gmail.com>
2016-02-20 16:47:23 -0800

    Add failing tests for the collection tooltips.

--- app/assets/javascripts/read/test/specs/collection-markers.js
+++ app/assets/javascripts/read/test/specs/collection-markers.js
@@ -10,6 +10,9 @@ import * as utils from '../utils';
 import addMarkersJSON from
 '../fixtures/collection-markers/collections/add-markers.json';
 
+import showTooltipsJSON from
+'../fixtures/collection-markers/collections/show-tooltips.json';
+
 
 describe('Collection Markers', function() {
 
@@ -43,6 +46,53 @@ describe('Collection Markers', function() {
       lng: 5,
       lat: 6,
     });
+
+  });
+
+
+  describe('shows a tooltip on hover', function() {
+
+
+    beforeEach(function() {
+      utils.respondCollections(showTooltipsJSON);
+    });
+
+
+    it('uses the town as the label, when possible', function() {
+
+      // Hover on collection with town.
+      markers.group.fire('mouseover', {
+        layer: markers.idToMarker[1]
+      });
+
+      expect($('.leaflet-popup-content')).toHaveText('town');
+
+    });
+
+
+    it('uses the county as the label, when possible', function() {
+
+      // Hover on collection with county.
+      markers.group.fire('mouseover', {
+        layer: markers.idToMarker[2]
+      });
+
+      expect($('.leaflet-popup-content')).toHaveText('county');
+
+    });
+
+
+    it('falls back to the province as the label', function() {
+
+      // Hover on collection with province.
+      markers.group.fire('mouseover', {
+        layer: markers.idToMarker[3]
+      });
+
+      expect($('.leaflet-popup-content')).toHaveText('province');
+
+    });
+
 
   });
 
--- spec/fixtures/collection_markers_spec.rb
+++ spec/fixtures/collection_markers_spec.rb
@@ -5,13 +5,59 @@ describe 'Collection Markers', type: :feature do
 
   it 'add-markers' do
 
-    create(:collection, id: 1, geometry: Helpers::Geo.point(1, 2))
-    create(:collection, id: 2, geometry: Helpers::Geo.point(3, 4))
-    create(:collection, id: 3, geometry: Helpers::Geo.point(5, 6))
+    create(
+      :collection,
+      id: 1,
+      geometry: Helpers::Geo.point(1, 2),
+    )
+
+    create(
+      :collection,
+      id: 2,
+      geometry: Helpers::Geo.point(3, 4),
+    )
+
+    create(
+      :collection,
+      id: 3,
+      geometry: Helpers::Geo.point(5, 6),
+    )
 
     visit('api/collections.json')
 
     write_collection_fixture('collection-markers', 'add-markers', page)
+
+  end
+
+  it 'show-tooltips' do
+
+    create(
+      :collection,
+      id: 1,
+      geometry: Helpers::Geo.point(0, 0),
+      town_p: 'town',
+      county_p: 'county',
+      province_p: 'province',
+    )
+
+    create(
+      :collection,
+      id: 2,
+      geometry: Helpers::Geo.point(0, 0),
+      county_p: 'county',
+      province_p: 'province',
+    )
+
+    create(
+      :collection,
+      id: 3,
+      geometry: Helpers::Geo.point(0, 0),
+      province_p: 'province',
+    )
+
+    visit('api/collections.json')
+
+    write_collection_fixture('collection-markers', 'show-tooltips', page)
 
   end
 
