c94ce11719751c9bd94b099a771cda19f2cae69a
David McClure <davidwilliammcclure@gmail.com>
2016-02-24 21:26:44 -0800

    Set the default choropleth from <Choropleth />.

--- app/assets/javascripts/read/src/components/choropleth.js
+++ app/assets/javascripts/read/src/components/choropleth.js
@@ -4,22 +4,36 @@ import _ from 'lodash';
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import * as actions from '../actions/map';
+
 import Component from './component';
 
 
 // TODO: set default.
 
 
-@connect(state => ({
-  code: state.counties.choropleth
-}))
+@connect(
+  state => ({
+    code: state.counties.choropleth
+  }),
+  actions
+)
 export default class extends Component {
 
 
   static propTypes = {
     g: PropTypes.array.isRequired,
     code: PropTypes.string,
+    changeChoropleth: PropTypes.func.isRequired,
   };
+
+
+  /**
+   * Show the variable.
+   */
+  componentDidMount() {
+    this.props.changeChoropleth('a100001_10');
+  }
 
 
   /**
--- app/assets/javascripts/read/src/reducers/map.js
+++ app/assets/javascripts/read/src/reducers/map.js
@@ -15,9 +15,7 @@ const initialState = {
 
   baseLayerId: null,
   wmsLayerId: null,
-
-  // TODO: Trigger this from <CountyChoropleth />.
-  choropleth: 'a100001_10',
+  choropleth: null,
 
   showMenu: false,
 
