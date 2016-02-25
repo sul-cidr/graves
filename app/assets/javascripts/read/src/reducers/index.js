9da10d6a02c390802cdebc71d7fb3327712e434a
David McClure <davidwilliammcclure@gmail.com>
2016-02-25 14:05:44 -0800

    Scrub out the old reducers.

--- app/assets/javascripts/read/src/reducers/index.js
+++ app/assets/javascripts/read/src/reducers/index.js
@@ -2,27 +2,21 @@
 
 import { combineReducers } from 'redux';
 
-import widgets from './widgets';
 import collections from './collections';
 import counties from './counties';
 import sections from './sections';
-import baseLayer from './base-layer';
-import wmsLayer from './wms-layer';
 
-import map from './map';
 import timeSlider from './time-slider';
+import map from './map';
 
 
 export default combineReducers({
 
-  widgets,
   collections,
   counties,
   sections,
-  baseLayer,
-  wmsLayer,
 
-  map,
   timeSlider,
+  map,
 
 });
