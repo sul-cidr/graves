481f26482f6139602bf7180f7dcf7152bd5ddd70
David McClure <davidwilliammcclure@gmail.com>
2016-02-25 14:49:41 -0800

    Block in the basic logic to render the timeline collections.

--- app/assets/javascripts/read/src/components/time-slider.js
+++ app/assets/javascripts/read/src/components/time-slider.js
@@ -1,19 +1,28 @@
 
 
-import { connect } from 'react-redux';
+import d3 from 'd3';
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 import Component from './component';
 
 
+@connect(state => ({
+  geojson: state.collections.geojson
+}))
 export default class extends Component {
 
 
+  static propTypes = {
+    geojson: PropTypes.object,
+  };
+
+
   /**
-   * Render the time slider.
+   * Create slider when mounted.
    */
   componentDidMount() {
-    console.log(this.refs.slider);
+    this.makeBrush();
   }
 
 
@@ -26,6 +35,53 @@ export default class extends Component {
         <div ref="slider"></div>
       </div>
     );
+  }
+
+
+  /**
+   * Create the brush slider.
+   */
+  makeBrush() {
+
+    // TODO
+
+    let container = d3.select(this.refs.slider);
+
+    let w = container.node().offsetWidth;
+    let h = 50;
+
+    let timeExtent = [
+      new Date(2000, 1, 1),
+      new Date(2016, 1, 1),
+    ];
+
+    let svg = container.append('svg')
+      .attr('width', w)
+      .attr('height', h);
+
+    let context = svg.append('g')
+      .classed('context', true);
+
+    let xAxis = d3.time.scale()
+      .range([0, w])
+      .domain(timeExtent);
+
+    let brush = d3.svg.brush()
+      .x(xAxis);
+
+    context.selectAll('circle.collection')
+
+      .data(this.props.geojson.features)
+      .enter()
+      .append('circle')
+
+      .attr('transform', function(d) {
+        let date = new Date(d.properties.notice.deadline);
+        return `translate(${xAxis(date)},${h/2})`
+      })
+
+      .attr('r', 5);
+
   }
 
 
