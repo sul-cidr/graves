f4103727193ee4be1b386bf7a1fcff51d4bab94c
David McClure <davidwilliammcclure@gmail.com>
2016-02-26 13:03:54 -0800

    Add a custom layout for the high voltage pages.

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -4,4 +4,7 @@ HighVoltage.configure do |config|
   # Un-prefixed routes for static pages.
   config.route_drawer = HighVoltage::RouteDrawers::Root
 
+  # Use the custom layout.
+  config.layout = 'page'
+
 end
