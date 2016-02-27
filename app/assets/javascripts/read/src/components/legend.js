174207a76c9233a8276f60d2dc10cec806880d79
David McClure <davidwilliammcclure@gmail.com>
2016-02-26 18:20:36 -0800

    Block in a collection legend.

--- app/assets/javascripts/read/src/components/legend.js
+++ app/assets/javascripts/read/src/components/legend.js
@@ -3,10 +3,12 @@
 import d3 from 'd3';
 import React from 'react';
 
+// TODO: browserify-shim?
 window.d3 = d3;
 import 'd3-svg-legend';
 
 import Component from './component';
+import { scale } from './collection-scale';
 
 
 export default class extends Component {
@@ -16,7 +18,25 @@ export default class extends Component {
    * Render the legend.
    */
   componentDidMount() {
-    // TODO
+
+    let svg = d3.select(this.refs.svg);
+
+    let g = svg.append('g')
+      .classed('legendSize', true)
+      .attr('transform', 'translate(0, 40)');
+
+    let legend = d3.legend.size()
+      .shape('circle')
+      .shapePadding(15)
+      .labelOffset(20)
+      .orient('horizontal')
+      .labelFormat(d3.format('0,000'))
+      .cells([7, 100, 1000, 10000])
+      .labels(['Unknown'])
+      .scale(scale);
+
+    g.call(legend);
+
   }
 
 
