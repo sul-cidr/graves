5e77bfb76992f1457d6d70120317918ad8f65add
David McClure <davidwilliammcclure@gmail.com>
2016-02-28 17:48:11 -0800

    Add basic auth in production mode.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate
+
+  #
+  # Basic auth in production.
+  #
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |u, p|
+        u == ENV['GRAVES_USER'] && p == ENV['GRAVES_PASS']
+      end
+    end
+  end
+
 end
