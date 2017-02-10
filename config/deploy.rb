be6eec173069aa2d72c4b0a15522e0ff9533e796
Scott Bailey <csb5t@virginia.edu>
2017-02-10 12:13:03 -0800

    Deployment fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
-
 set :application, 'graves'
 set :repo_url, 'https://github.com/sul-cidr/graves.git'
 set :user, 'cidr'
@@ -17,21 +14,6 @@ server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web 
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push(
@@ -55,19 +37,3 @@ set :linked_files, fetch(:linked_files, []).push(
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-# Install devDependencies
-set :npm_flags, '--silent --no-spin'
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
