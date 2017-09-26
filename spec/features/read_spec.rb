b4a3c0e626d3181680039453a87535f717ce6d62
Javier de la Rosa <versae@gmail.com>
2017-09-26 16:56:12 +0200

    Fixes #186. Adding a new request parameter to collections.json to filter by tags.

--- spec/features/read_spec.rb
+++ spec/features/read_spec.rb
@@ -2,9 +2,7 @@
 require 'rails_helper'
 
 describe '/read/:slug', type: :feature do
-
   it 'links to the narratives' do
-
     n = create(:narrative)
 
     visit("read/#{n.slug}")
@@ -18,8 +16,32 @@ describe '/read/:slug', type: :feature do
     # Check for the raw narrative markup.
     markup = Nokogiri::HTML(page.body).css('#narrative')
     expect(markup.children.to_s).to eq(n.markup)
-
-
   end
 
+  it 'filters collections based on one tag' do
+    tag1 = create(:tag, label: 'Tag 1', tag: 'tag1')
+    tag2 = create(:tag, label: 'Tag 2', tag: 'tag2')
+    create(:collection, tags: [tag1])
+    visit("api/collections.json?tag=#{tag1.tag}")
+    expect(page).to have_content(tag1.tag)
+    expect(page).not_to have_content(tag2.tag)
+  end
+
+  it 'filters collections based on several tags' do
+    tag1 = create(:tag, label: 'Tag 1', tag: 'tag1')
+    tag2 = create(:tag, label: 'Tag 2', tag: 'tag2')
+    create(:collection, tags: [tag1, tag2])
+    visit("api/collections.json?tag=#{tag1.tag},#{tag2.tag}")
+    expect(page).to have_content(tag1.tag)
+    expect(page).to have_content(tag2.tag)
+  end
+
+  it 'returns all collections when tag is invalid' do
+    tag1 = create(:tag, label: 'Tag 1', tag: 'tag1')
+    tag2 = create(:tag, label: 'Tag 2', tag: 'tag2')
+    create(:collection, tags: [tag1, tag2])
+    visit('api/collections.json?tag=invalid')
+    expect(page).to have_content(tag1.tag)
+    expect(page).to have_content(tag2.tag)
+  end
 end
