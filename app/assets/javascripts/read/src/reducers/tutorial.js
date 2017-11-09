9ac828f37aa9788fe738d0418c6daf64acd06719
Scott Bailey <csb5t@virginia.edu>
2017-11-09 13:58:12 -0800

    Add button to show tutorial modal from map

--- app/assets/javascripts/read/src/reducers/tutorial.js
+++ app/assets/javascripts/read/src/reducers/tutorial.js
@@ -1,35 +1,27 @@
+import { createReducer } from "../utils";
 
-
-import { createReducer } from '../utils';
-
-
-import {
-  CLOSE_TUTORIAL_MODAL,
-} from '../constants';
-
+import { CLOSE_TUTORIAL_MODAL, OPEN_TUTORIAL_MODAL } from "../constants";
 
 const initialState = {
-
-  showTutorialModal: tutorialState(),
-
+  showTutorialModal: tutorialState()
 };
 
 function tutorialState() {
-  if (sessionStorage.getItem('tutorialModal') == 'false') {
+  if (sessionStorage.getItem("tutorialModal") == "false") {
     return false;
   } else {
     return true;
   }
-};
-
+}
 
 const handlers = {
-
   [CLOSE_TUTORIAL_MODAL]: () => ({
-    showTutorialModal: false,
+    showTutorialModal: false
   }),
 
+  [OPEN_TUTORIAL_MODAL]: () => ({
+    showTutorialModal: true
+  })
 };
-
 
 export default createReducer(initialState, handlers);
